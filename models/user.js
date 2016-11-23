affe55c6d8002e6f2cc20cbe8c040dfaf8502312
iliass <iliass24@gmail.com>
2016-11-22 22:16:06 -0500

    add navigation slider for mobile

--- models/user.js
+++ models/user.js
@@ -7,15 +7,15 @@ module.exports = (sequelize) => {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true,
-      },
+        notEmpty: true
+      }
     },
     lastName: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true,
-      },
+        notEmpty: true
+      }
     },
     username: {
       type: Sequelize.STRING,
@@ -23,8 +23,8 @@ module.exports = (sequelize) => {
       primaryKey: true,
       validate: {
         notEmpty: true,
-        isAlphanumeric: true,
-      },
+        isAlphanumeric: true
+      }
     },
     email: {
       type: Sequelize.STRING,
@@ -32,32 +32,32 @@ module.exports = (sequelize) => {
       unique: true,
       validate: {
         notEmpty: true,
-        isEmail: true,
-      },
+        isEmail: true
+      }
     },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
-        notEmpty: true,
-      },
+        notEmpty: true
+      }
     },
 
      sportspref: {
      type: Sequelize.STRING,
-     allowNull: true,
+     allowNull: false,
      validate: {
        notEmpty: true,
-       isAlphanumeric: true,
-     },
+       isAlphanumeric: true
+     }
     },
    zipcode: {
      type: Sequelize.STRING,
      allowNull: false,
      validate: {
        notEmpty: true,
-       isAlphanumeric: true,
-     },
+       isAlphanumeric: true
+     }
   }
 });
 
@@ -69,7 +69,7 @@ module.exports = (sequelize) => {
     }).then((hashedPw) => {
       user.password = hashedPw;
     })
-  );
+  )
 
   return User;
 };
